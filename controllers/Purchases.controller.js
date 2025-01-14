aadb92576ece64dc0e44fba7f213115660983635
Shekhar-rana007 <ranashekhar007@gmail.com>
2025-01-14 17:55:31 +0530

    purchase history created

--- controllers/Purchases.controller.js
+++ controllers/Purchases.controller.js
@@ -1,46 +1,92 @@
 const { Purchase } = require("../models/Purchase");
 
 class PurchaseController {
+  async create(req, res) {
+    const data = req.body;
+    const newData = { ...data, user_id: req?.user._id };
+    await Purchase.create(newData);
+    return res.status(201).json({ message: "Purchase Order Gererated" });
+  }
 
-    async create(req,res){
-        const data = req.body;
-        const newData = {...data,user_id:req?.user._id}
-        await Purchase.create(newData);
-        return res.status(201).json({message:"Purchase Order Gererated"})
-    }
+  async getAll(req, res) {
+    const data = await Purchase.find({}).populate(
+      "user_id customer_id product_name assined_to"
+    );
+    return res.status(200).json({ message: "all purchases order found", data });
+  }
 
-    async getAll (req,res){
-        const data = await Purchase.find({}).populate("user_id").populate("customer_id").populate("product_name").populate("assined_to");
-        return res.status(200).json({message:"all purchases order found",data})
-    }
+  async getOne(req, res) {
+    const { id } = req.params;
+    const data = await Purchase.findById(id).populate(
+      "user_id customer_id product_name assined_to"
+    );
+    return res.status(200).json({ message: "data found by id", data });
+  }
 
-    async getOne (req,res){
-        const {id} = req.params;
-        const data = await Purchase.findById(id).populate("user_id").populate("customer_id").populate("product_name").populate("assined_to");
-        return res.status(200).json({message:"data found by id",data});
-    }
+  async CustomerGet(req, res) {
+    const data = await Purchase.aggregate([
+      {
+        $match: {
+          customer_id: req?.user?._id,
+        },
+      },
+      {
+        $lookup: {
+          from: "products",
+          foreignField: "_id",
+          localField: "product_name",
+          as: "product_name",
+          pipeline: [
+            {
+              $lookup: {
+                from: "production-processes",
+                foreignField: "item",
+                localField: "_id",
+                as: "process",
+                pipeline:[
+                    {
+                        $project:{
+                            processes:1
+                        }
 
-    async update(req,res){
-        const data = req.body;
-        const {id} = req.params;
-       const find =  await Purchase.findById(id);
-        if(!find){
-            return res.status(400).json({message:"data not found"});
-        }
-        await Purchase.findByIdAndUpdate(id,data)
-        return res.status(201).json({message:"Purchase Order updated"})
-    }
-    
-    async Delete(req,res){
-        const {id} = req.params;
-       const find =  await Purchase.findById(id);
-        if(!find){
-            return res.status(400).json({message:"data not found"});
-        }
-        await Purchase.findByIdAndDelete(id)
-        return res.status(201).json({message:"Purchase Order deleted"})
-    }
+                    }
+                ]
+              },
+            },
+            {
+                $project:{
+                    name:1,
+                    process:1
+                }
+            }
+            
+          ],
+        },
+      },
+    ]);
+    return res.status(200).json({ message: "customer data found", data });
+  }
 
+  async update(req, res) {
+    const data = req.body;
+    const { id } = req.params;
+    const find = await Purchase.findById(id);
+    if (!find) {
+      return res.status(400).json({ message: "data not found" });
+    }
+    await Purchase.findByIdAndUpdate(id, data);
+    return res.status(201).json({ message: "Purchase Order updated" });
+  }
+
+  async Delete(req, res) {
+    const { id } = req.params;
+    const find = await Purchase.findById(id);
+    if (!find) {
+      return res.status(400).json({ message: "data not found" });
+    }
+    await Purchase.findByIdAndDelete(id);
+    return res.status(201).json({ message: "Purchase Order deleted" });
+  }
 }
 
-exports.purchaseController =  new PurchaseController()
+exports.purchaseController = new PurchaseController();
--- routes/Purchases.routes.js
+++ routes/Purchases.routes.js
@@ -3,12 +3,14 @@ const { purchaseController } = require("../controllers/Purchases.controller");
 const { Validater } = require("../helper/checkvalidation");
 const { PurchasesValidation } = require("../validations/purchase.validation");
 const { isAuthenticated } = require("../middlewares/isAuthenticated");
+const { isCustomerAuthenticated } = require("../middlewares/Customer.middleware");
 
 const route = Router()
 
 route.post("/create",isAuthenticated,Validater(PurchasesValidation),purchaseController.create)
 route.get("/getAll",isAuthenticated,purchaseController.getAll)
 route.get("/get/:id",isAuthenticated,purchaseController.getOne)
+route.get("/customer-get",isCustomerAuthenticated,purchaseController.CustomerGet)
 route.put("/update/:id",isAuthenticated,Validater(PurchasesValidation),purchaseController.update)
 route.delete("/update/:id",isAuthenticated,purchaseController.Delete)
 
